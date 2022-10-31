dac75270054a7df26fb5534e45501db961dac8fb
Tim Quigg <timjquigg@gmail.com>
2022-10-31 14:52:44 -0600

    Added a route for displaying a basic Hello World message with HTML

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+// The goal of this exercise is to implement a basic web server using Express.
+
 const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080;
@@ -7,12 +9,17 @@ const urlDatabase = {
   '9sm5xK': 'http:// www.google.com'
 };
 
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
