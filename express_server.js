e658fbd494f122ef69865800f2bbfc73dfc2ae16
Tim Quigg <timjquigg@gmail.com>
2022-10-31 16:13:50 -0600

    Added functionality to accept an url id in the path and pass the id & corresponding website address from the urlDatabase to the urls_show.ejs template and send that the the client.

--- express_server.js
+++ express_server.js
@@ -16,14 +16,24 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render('urls_show', templateVars);
 });
+
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
 
 app.listen(PORT, () => {
-  console.log('Example app listening on port ${PORT}!');
+  console.log(`Example app listening on port ${PORT}!`);
 });
