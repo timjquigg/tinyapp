152604bbbd37472693150ee688d66a5e18c977f2
Tim Quigg <timjquigg@gmail.com>
2022-11-01 14:09:58 -0600

    Added functionality to delete URLs from list.

--- express_server.js
+++ express_server.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 8080; // default port 8080;
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -21,41 +20,67 @@ const generateRandomString = () => {
     let randomChar = string[Math.floor(Math.random() * string.length)];
     randomNumbers.push(randomChar);
   }
-
+  
   return randomNumbers.join('');
 };
 
-// Routes
+//
+// MIDDLEWARE
+//
+app.use(express.urlencoded({ extended: true }));
+
+//
+// ROUTES
+//
+
+// Home
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+
+// Index of URLs
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
+// Create new URL
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// Open particular URL by short URL :id
 app.get('/urls/:id', (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
+// Redirect to longURL corresponding to short URL :id
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
+//
+// CREATE
+//
+
+// Create new short URL
 app.post('/urls', (req, res) => {
-  console.log(req.body);
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
+//
+// DELETE
+//
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.body.id];
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
